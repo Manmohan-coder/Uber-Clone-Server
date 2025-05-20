d806fb69c52a55023d524357839c3c0b84bc2dd6
Manmohan-coder <manmohanj8268@gmail.com>
2025-05-21 00:21:24 +0530

    basic code + db config

---
+++ app.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import { connectDB } from './config/db.config.js';
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));    
+
+export default app;
